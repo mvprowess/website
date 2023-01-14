1e337bf915dafb1ff67bc5f507b612546212c50d
FMJdev <info@fmj.dev>
2023-01-15 00:54:19 +0100

    initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Libre_Franklin } from '@next/font/google';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const libreFranklin = Libre_Franklin({
+  weight: '700',
+  style: 'normal',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={libreFranklin.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
